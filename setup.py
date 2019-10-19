452074a9c06c678c4dfcecb544ca89bf99f1fae8
cbedroid <cbedroid1614@gmail.com>
2019-10-19 15:39:23 +0100

    travis fix 2

---
+++ setup.py
@@ -0,0 +1,11 @@
+from seuptools import setup,find_packages
+
+setup( name='Datpiff',
+       version='1.0.1',
+       decription='Datpiff Mixtape player',
+       author='Cornelius Brooks',
+       author_email='cbedroid1614@gmail.com',
+       url='https://github.com/cbedroid/Datpiff',
+       packages=find_packages(),
+     )
+
