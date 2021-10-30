4228d45aeb0d103d3bcccf7c8ff811a7fa773385
cbedroid <cbedroid1614@gmail.com>
2021-10-30 12:35:38 -0400

    Fixes #14 Refactor and removed all bad test. (in progress)

--- tests/Mixtapes/test_mixsetup.py
+++ tests/Mixtapes/test_mixsetup.py
@@ -15,26 +15,46 @@ class TestMixSetup(BaseTest, TestCase):
         response = self.mocked_response(content=content)
         self.DOM = mixsetup.DOMProcessor(response)
 
-    def test_mixtapes_total(self):
+    def test_mixtapes_total_is_correct(self):
         hot_category_url = Urls.category["hot"]
-        total_mixtapes = self.DOM._get_mixtapes_total(hot_category_url)
+        self.DOM._parse_mixtape_page(hot_category_url)
+        total_mixtapes = self.DOM.total_mixtapes
 
-        self.assertEqual(total_mixtapes, 12)
+        self.assertEqual(total_mixtapes, 24)
 
-    def test_mixsetup_get_page_links_return_mixtapes_pages(self):
-        page_links = self.DOM.get_page_links
+    def test_mixsetup_return_no_mixtapes_when_Datpiff_mixtapes_(self):
+        # mocked _parse_mixtapes request response text so
+        # BeautifulSoup can not find the mixtape main container and exit early
+        method = self.DOM._session.method = Mock(autospec=True)
+        method.text = "<html></html>"
+        hot_category_url = Urls.category["hot"]
+
+        # reset the total mixtapes since self.DOM is already initialized
+        del self.DOM._total_mixtapes
+        self.DOM._parse_mixtape_page(hot_category_url)
+        total_mixtapes = self.DOM.total_mixtapes
+        self.assertEqual(total_mixtapes, 0)
+
+    def test_mixsetup_find_mixtapes_pages_links(self):
+        page_links = self.DOM._get_page_links()
         self.assertGreaterEqual(len(page_links), 1)
 
-    def test_get_HtmlResponse_returns_content(self):
+    def test_mixtsetup_get_request_method_return_valid_html_content(self):
         url = Urls.category["hot"]
-        html = self.DOM._getHtmlResponse(url)
+        html = self.DOM._request_get(url)
         self.assertIsNotNone(html)
+        self.assertIn("contentListing", html)
+        self.assertIn("contentItem", html)
 
-    def test_get_HtmlResponse_contains_correct_content(self):
+    def test_mixsetup_mixtapes_page_contains_correct_artists_and_mixtapes(self):
         url = Urls.category["hot"]
-        html = self.DOM._getHtmlResponse(url)
+        html = self.DOM._request_get(url)
 
         mixtapes_and_artists = zip(self.mixtape_list, self.artist_list)
         for mixtape, artist in mixtapes_and_artists:
             self.assertIn(mixtape, html)
             self.assertIn(artist, html)
+
+    def test_mixsetup_get_page_links_return_page_links(self):
+        url_links = self.DOM._get_page_links()
+        self.assertGreaterEqual(len(url_links), 1)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,15 +5,31 @@ PATH = os.path.dirname(os.path.abspath(__file__))
 
 
 class BaseTest:
-
     # mixtapes and artists that are included in "mixtape" fixtures. see fixture/mixtape.html
     mixtape_list = ["A Gangsta's Pain: Reloaded", "Folarin II", "The Butterfly Effect"]
     artist_list = ["Moneybagg Yo", "Wale", "Fetty Wap"]
 
+    # fmt: off
+    # songs from MoneyBagg Yo - A Gangsta's Pain: Reloaded
+    song_list = [
+        "Switches  Dracs (feat. Lil Durk  EST Gee)", "Wat Be Wrong", "Gave It (feat. Big Homiie G)",
+        "This Feeling (feat. Yung Bleu  Ja'niyah)", "Scorpio", "Another One (feat. DJ Khaled)",
+        "Wockesha (Remix) (feat. Lil Wayne  Ashanti)", "Memphganistan (feat. Kaash Paige)", "Just Say Det",
+        "GO! (feat. BIG30)", "Wockesha", "Shottas (Lala)",
+        "Hard For The Next (feat. Future)", "If Pain Was A Person", "I Believe U (feat. TripStar)",
+        "Time Today", "Interlude", "Free Promo (feat. Polo G  Lil Durk)",
+        "Hate It Here", "Love It Here", "Clear Da Air",
+        "Projects", "One Of Dem Nights (feat. Jhene Aiko)", "FR",
+        "Certified Neptunes (feat. Pharrell Williams)",
+        "Change Da Subject", "Least Ian Lie", "Bipolar Virgo",
+        "A Gangsta's Pain",
+    ]
+    # fmt: on
+
     # mixtape's search testing parameter
     mixtape_search_parameter = {"search": "Jay-Z"}
 
-    def get_request_content(self, namespace="mixtape"):
+    def get_request_content(self, namespace="mixtape", mode="r"):
         """Return testing web page content
 
         Raises:
@@ -25,14 +41,16 @@ class BaseTest:
         if not os.path.isfile(file):
             raise FileExistsError("{} test file not found".format(namespace))
 
-        with open(file, "r") as pf:
+        with open(file, mode) as pf:
             return pf.read()
 
-    def mocked_response(self, status=200, content="", json=None):
+    def mocked_response(self, status=200, content="", json=None, **kwargs):
         session = Mock()
         session.raise_for_status = Mock()
         session.status_code = status
         session.text = content
-        session.content = content
+        session.content = str(content).strip().encode("utf-8")
         session.json = json or {}
+        for k, v in kwargs.items():
+            setattr(session, k, v)
         return session
