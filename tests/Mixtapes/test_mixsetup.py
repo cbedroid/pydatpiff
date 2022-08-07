3ad313520df03a65bcc93a44fc000ec75d625b16
corbrook <corbrook@cisco.com>
2022-08-07 00:41:09 -0400

    updated tests to match refactored project methods

--- tests/Mixtapes/test_mixsetup.py
+++ tests/Mixtapes/test_mixsetup.py
@@ -1,19 +1,19 @@
 from unittest import TestCase
 from unittest.mock import Mock
 
-from pydatpiff.backend import mixsetup
+from pydatpiff.backend import scraper
 from pydatpiff.urls import Urls
 from tests.test_utils import BaseTest
 
 
-class TestMixSetup(BaseTest, TestCase):
+class Testscaper(BaseTest, TestCase):
     def setUp(self):
         content = self.get_request_content("mixtape")
-        method = mixsetup.Session.method = Mock(autospec=True)
+        method = scraper.Session.method = Mock(autospec=True)
         method.return_value = self.mocked_response(content=content)
 
         response = self.mocked_response(content=content)
-        self.DOM = mixsetup.DOMProcessor(response)
+        self.DOM = scraper.MixtapeScraper(response)
 
     def test_mixtapes_total_is_correct(self):
         hot_category_url = Urls.category["hot"]
@@ -22,7 +22,7 @@ class TestMixSetup(BaseTest, TestCase):
 
         self.assertEqual(total_mixtapes, 24)
 
-    def test_mixsetup_return_no_mixtapes_when_Datpiff_mixtapes_(self):
+    def test_mixtape_scraper_return_no_mixtapes_when_Datpiff_mixtapes_(self):
         # mocked _parse_mixtapes request response text so
         # BeautifulSoup can not find the mixtape main container and exit early
         method = self.DOM._session.method = Mock(autospec=True)
@@ -35,18 +35,18 @@ class TestMixSetup(BaseTest, TestCase):
         total_mixtapes = self.DOM.total_mixtapes
         self.assertEqual(total_mixtapes, 0)
 
-    def test_mixsetup_find_mixtapes_pages_links(self):
+    def test_mixtape_scaper_find_mixtapes_pages_links(self):
         page_links = self.DOM._get_page_links()
         self.assertGreaterEqual(len(page_links), 1)
 
-    def test_mixtsetup_get_request_method_return_valid_html_content(self):
+    def test_mixtape_scraper_get_request_method_return_valid_html_content(self):
         url = Urls.category["hot"]
         html = self.DOM._request_get(url)
         self.assertIsNotNone(html)
         self.assertIn("contentListing", html)
         self.assertIn("contentItem", html)
 
-    def test_mixsetup_mixtapes_page_contains_correct_artists_and_mixtapes(self):
+    def test_mixtape_scraper_has_mixtapes_page_contains_correct_artists_and_mixtapes(self):
         url = Urls.category["hot"]
         html = self.DOM._request_get(url)
 
@@ -55,6 +55,6 @@ class TestMixSetup(BaseTest, TestCase):
             self.assertIn(mixtape, html)
             self.assertIn(artist, html)
 
-    def test_mixsetup_get_page_links_return_page_links(self):
+    def test_mixtape_scraper_get_page_links_return_page_links(self):
         url_links = self.DOM._get_page_links()
         self.assertGreaterEqual(len(url_links), 1)
