2479c84f2974dbcb2c127c9bedc1af256abdab70
corbrook <corbrook@cisco.com>
2022-08-07 00:43:27 -0400

    Updated setup.py build

--- pydatpiff/__init__.py
+++ pydatpiff/__init__.py
@@ -1,2 +1,2 @@
 """ Pydatpif Version Control """
-__version__ = "2.1.0"
+__version__ = "2.0.0"
--- setup.py
+++ setup.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python
-import setuptools
 
-setuptools.setup()
+from setuptools import find_packages, setup
+
+setup(
+    name="Pydatpiff",
+    version="2.0.0",
+    author="cbedroid",
+    author_email="cbedroid1614@example.com",
+    packages=find_packages(
+        where=".",
+        include=["pydatpiff*"],  # ["*"] by default
+        exclude=["pydatpiff.tests"],  # empty by default
+    ),
+    url="https://pypi.org/project/pydatpiff/",
+    license="LICENSE",
+    description="Unofficial Datpiff Mixtape player - Download and play the newest Hip-Hop and RnB Songs.",
+    long_description=open("README.md").read(),
+    install_requires=open("requirements.txt").read(),
+)
