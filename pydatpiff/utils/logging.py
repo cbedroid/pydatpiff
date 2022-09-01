9e54cd6a85d81e6cb08d7f118f6c240a68ec1828
cbedroid <cbedroid1614@gmail.com>
2022-09-01 01:11:54 -0400

    Moved logging config in utils directory

--- pydatpiff/utils/logging.py
+++ pydatpiff/utils/logging.py
@@ -1,12 +1,10 @@
-import logging
+import logging  # noqa
 import logging.config
-import os
 
 import yaml
 
-DIR = os.path.dirname(os.path.abspath(__file__))
-file_name = os.path.join(DIR, "../.logging_config.yaml")
+log_config_file = "./.logging_config.yaml"
 
-with open(file_name, "r") as f:
+with open(log_config_file, "r") as f:
     config = yaml.load(f.read(), Loader=yaml.FullLoader)
     logging.config.dictConfig(config)
